16a162d0e260d69a079b500701dd5dd378b59ef0
vergaloy <42758185+vergaloy@users.noreply.github.com>
2021-03-22 19:25:31 +0900

    fix warning message by missing round

--- cnmfe/get_PNR_coor_greedy_PV.m
+++ cnmfe/get_PNR_coor_greedy_PV.m
@@ -1,7 +1,7 @@
 function [cn_all,pnr_all,cn,pnr]=get_PNR_coor_greedy_PV(Y,Gsig)
 
 si=size(Y);
-le=linspace(1,si(3),round(si(3)/1000)+1);
+le=round(linspace(1,si(3),round(si(3)/1000)+1));
 cn_all=zeros(si(1),si(2),size(le,2)-1);
 pnr_all=zeros(si(1),si(2),size(le,2)-1);
 
